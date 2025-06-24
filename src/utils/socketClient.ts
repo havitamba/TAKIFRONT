2f7f0ebfadbd62c36c83f8fbb6aab346159d5aeb
havitamba <elielielia1@gmail.com>
2025-06-24 15:15:44 +0300

    sdfsd

--- src/utils/socketClient.ts
+++ src/utils/socketClient.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socketClient = io("http://takiback-production.up.railway.app", {
+export const socketClient = io("https://takiback-production.up.railway.app", {
   transports: ["websocket"], // Prefer WebSocket transport
 });
